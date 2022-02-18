9d8eb345625ff94ed6a18a77c784aafaca7d56bd
neil-toscano <75841666+neil-toscano@users.noreply.github.com>
2022-02-18 10:05:04 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$contra_entrar="1234";
+$usu_entrar="Juan";
+
+$el_usuario=isset($_GET['usuario']) ? $_GET['usuario'] : $_POST['usuario'];
+$la_contra=isset($_GET['contra']) ? $_GET['contra'] : $_POST['contra'];
+
+if ($el_usuario==$usu_entrar && $la_contra==$contra_entrar) {
+  echo 'autorizado';
+} else {
+  echo 'fallo';
+}
+
+
+
+?>
